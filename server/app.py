8cadae722bcf73eddc5379e5d291e2177561ad89
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-21 09:38:20 +0300

    Attempted assignment

--- server/app.py
+++ server/app.py
@@ -15,20 +15,87 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return '<h1>Zoo app</h1>'
+    return '<h1>Welcom to the Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+
+    animal = Animal.query.filter(Animal.id==id).first()
+
+    if not animal:
+        response_body = f"<h1>404 error. Animal not found</h1>"
+        response = make_response(response_body,404)
+        return response
+    
+    response_body = f'''
+         
+        <ul>
+            <li>ID: {animal.id}</li>
+            <li>Name: { animal.name }</li>
+            <li>Species: { animal.species }</li>
+            <li>Zookeeper: { animal.zookeeper.name }</li>
+            <li>Enclosure: { animal.enclosure.environment }</li>
+        </ul>'''
+    
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter_by(id=id).first()
+
+    if not zookeeper:
+        response_body = f'<h1>404 error. Zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+
+    response_body = f''' 
+        <h1>Zookeeper Details</h1>
+        <ul>
+            <li>Name: {zookeeper.name}</li>
+            <li>Birthday: {zookeeper.birthday}</li>
+            <li>Animals Cared for by the Zookeeper:
+                <ul>
+        '''
+    for animal in zookeeper.animals:
+        response_body += f'<li>{animal.name}, {animal.species}</li>'
+
+    response_body += '''
+                </ul>
+            </li>
+        </ul>
+    '''
+
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter_by(id=id).first()
 
+    if not enclosure:
+        response_body = f'<h1>404 error. Enclosure not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+
+    animals = enclosure.animals
+    animals_list = [f'<li>{animal.name}, {animal.species}</li>' for animal in animals]
+
+    response_body = f'''
+         
+        <ul>
+            <li>ID: {enclosure.id}</li>
+            <li>Environment: { enclosure.environment }</li>
+            <li>Open to Visitors: { enclosure.open_to_visitors }</li>
+            <li>Animals in the enclosure:
+                <ul>{''.join(animals_list)} </ul>
+            </li>
+        </ul>'''
+    response = make_response(response_body, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
